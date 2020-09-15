3934802094b57bb4d69dadd3d5a44fc3873c0833
implement1 <50425206+implement1@users.noreply.github.com>
2020-09-15 03:59:16 -0400

    c

--- src/app/current-weather/current-weather.component.spec.ts
+++ src/app/current-weather/current-weather.component.spec.ts
@@ -1,7 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { WeatherService } from '../weather/weather.service'
 import { CurrentWeatherComponent } from './current-weather.component'
 
 describe('CurrentWeatherComponent', () => {
