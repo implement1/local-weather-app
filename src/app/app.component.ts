8d1bb4e6d186aab2250945b846946bdc8034a1c4
implement1 <50425206+implement1@users.noreply.github.com>
2020-09-15 10:16:43 -0400

    c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,17 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  template: `
+    <div style = "text-align:center" >
+      <h1>
+      LocalCast Weather
+    < / h1 >
+  <div>Your city, your forecast, right now! < /div>
+  < h2 > Current Weather < /h2>
+  < app - current - weather > </app-current-weather>
+
+< /div>
+`,
 })
 export class AppComponent {
   title = 'local-weather-app'
